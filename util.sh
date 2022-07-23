5d52b770d1fe14ee157444b043a503e76a1edbe5
Jerry Ng <ngshiheng@gmail.com>
2022-07-23 09:54:08 +0800

    refactor: move checks to util.sh

--- util.sh
+++ util.sh
@@ -2,5 +2,21 @@
 
 # log writes message to stdout with a timestamp
 log() {
-    echo "$(date -u): $1" | tee -a $LOG_LOCATION
+    echo "$(date -u): $1"
+}
+
+# check if command is installed
+check_command_installation() {
+    if ! command -v $1 &>/dev/null; then
+        log "$1 is not installed"
+        exit 0
+    fi
+}
+
+# check if Ghost is running
+check_ghost_status() {
+    if ! ghost status &>/dev/null; then
+        log "Ghost is not running"
+        exit 0
+    fi
 }
