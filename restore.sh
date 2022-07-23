fe66995ce5921bd1f041e40c1a8caa2ea7f29da3
Jerry Ng <ngshiheng@gmail.com>
2022-07-23 09:54:14 +0800

    feat: add backup restore script

---
+++ restore.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source util.sh
+
+# restore MySQL database
+# assumes that Ghost is installed
+restore_mysql() {
+    log "Restoring MySQL database..."
+    cd "$GHOST_DIR" || exit
+
+    mysql_user=$(ghost config get database.connection.user | tail -n1)
+    mysql_password=$(ghost config get database.connection.password | tail -n1)
+    mysql_database=$(ghost config get database.connection.database | tail -n1)
+
+    check_mysql_connection
+
+    log "Restoring MySQL database..."
+    gunzip -c "$GHOST_MYSQL_BACKUP_FILENAME" | mysql -u"$mysql_user" -p"$mysql_password" "$mysql_database"
+}
+
+# restore Ghost content folder
+restore_ghost_content() {
+    log "Restoring Ghost content..."
+    cd "$GHOST_DIR" || exit
+
+    tar -xzf "$GHOST_CONTENT_BACKUP_FILENAME"
+}
